77829c469e304dc94dd5dafc2f344aee498a00d6
Muhamad Assar <m.assar@outlook.com>
2024-06-06 18:42:27 +0300

    first draft

--- src/app/link/link.component.ts
+++ src/app/link/link.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
-  selector: 'app-link',
-  standalone: true,
-  imports: [],
-  templateUrl: './link.component.html',
-  styleUrl: './link.component.scss'
+    selector: 'app-link',
+    standalone: true,
+    imports: [NgOptimizedImage, TooltipModule],
+    templateUrl: './link.component.html',
+    styleUrl: './link.component.scss',
 })
 export class LinkComponent {
-
+    image = input<string>('image');
+    name = input<string>();
+    description = input<string>();
+    android = input<string>();
+    ios = input<string>();
+    mac = input<string>();
+    windows = input<string>();
 }
